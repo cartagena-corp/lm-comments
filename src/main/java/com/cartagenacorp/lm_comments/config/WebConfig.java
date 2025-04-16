bb5f78c23c2f1a9153e94d6537ea4beb82da76ec
Diego-Pedrozo <diegopedrozovillarreal@gmail.com>
2025-04-15 21:23:56 -0500

    feat: add custom CORS configuration using WebMvcConfigurer

--- src/main/java/com/cartagenacorp/lm_comments/config/WebConfig.java
+++ src/main/java/com/cartagenacorp/lm_comments/config/WebConfig.java
@@ -1,15 +1,29 @@
 package com.cartagenacorp.lm_comments.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${app.cors.allowed-origins}")
+    private String[] allowedOrigins;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
                 .addResourceHandler("/uploads/**")
                 .addResourceLocations("file:uploads/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
