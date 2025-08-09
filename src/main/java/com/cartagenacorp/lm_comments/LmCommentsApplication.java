0e7959f45c8f0f9c7fe834cac399c53df618b38c
Diego-Pedrozo <diegopedrozovillarreal@gmail.com>
2025-08-09 09:34:48 -0500

    build: Generatin .war File instead of .jar

--- src/main/java/com/cartagenacorp/lm_comments/LmCommentsApplication.java
+++ src/main/java/com/cartagenacorp/lm_comments/LmCommentsApplication.java
@@ -2,12 +2,19 @@ package com.cartagenacorp.lm_comments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LmCommentsApplication {
+public class LmCommentsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LmCommentsApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LmCommentsApplication.class);
 	}
 
 }
